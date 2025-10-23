6d9496224011c72bf5cc91171e6d8b0ce4cb2a17
Creeper_musume <141794440+EndeeerMan@users.noreply.github.com>
2025-10-23 20:38:52 +0800

    Add files via upload

--- 不限数量的长整型数排序/sorting.c
+++ 不限数量的长整型数排序/sorting.c
@@ -2,50 +2,60 @@
 #include <stdlib.h>
 
 int main(){
-    long long *arr = NULL;
-    long long num = 0;
-    long size = 0;
-    long capacity = 1;
-    long long swapper = 0;
+    long long *arr = NULL;//Define a pointer with NULL.
+    long long num = 0;//Pass the number into *arr.
+    long size = 0; //Actually size that user has input.
+    long capacity = 1; //Initial capacity of *arr.
+    long long swapper = 0; //Bubble sorting numbers swapper.
+
     //Memory allocation initialization.
+
     arr = (long long *)malloc(capacity * sizeof(long long));
-    if(arr == NULL){
+
+    if(arr == NULL){  //If allocate memory failed, the program exit with return value 1.
         printf("FATAL ERROR: Failed to allocate memory!\n");
         return 1;
     }else{
-        printf("Successfully allocated memory! (8 Bytes)\nNow Please enter the integers that you want to sort, and end with EOF or any other chars + enter:\n");
+        printf("Successfully allocated memory! (8 Bytes)\nNow Please enter the integers that you want to sort, and end with Ctrl + D , then enter:\n");
     }
+    //Detect user input.
     while(scanf("%lld",&num) == 1){
         if(capacity <= size){
-            capacity *= 2;
+            capacity *= 2; //If capacity not enough, extend memory with double capacity.
             arr = realloc(arr,capacity*sizeof(long long));
             if(arr == NULL){
-                printf("FATAL ERROR: Failed to allocate memory!\n");
+                printf("FATAL ERROR: Failed to extend memory!\n");
                 free(arr);
                 return 1;
             }
         }
         arr[size++] = num;
     }
+    
+    printf("Detected numbers: ");
     for(int i=0;i<size;i++){
         printf("%lld ",arr[i]);
     }
     printf("\n\n");
-    arrange:for(int i=0;i<size-1;i++){
+
+    arrange:for(int i=0;i<size-1;i++){   //Bubble sorting.
                 while(arr[i]>arr[i+1]){
                     swapper = arr[i];
                     arr [i] = arr[i+1];
                     arr[i+1] = swapper;
                 }
             }
-    for(int i=0;i<size-1;i++){
+
+    for(int i=0;i<size-1;i++){ //Detect numbers sequence correct or not.
         if(arr[i]>arr[i+1]){
-            goto arrange;
+            goto arrange; //If not, sort again.
         }
     }
+
     for(int i=0;i<size;i++){
         printf("%lld\n",arr[i]);
     }
-    free(arr);
+
+    free(arr); //Release memory resources.
     return 0;
 }
