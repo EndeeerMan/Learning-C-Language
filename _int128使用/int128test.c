6f62baeacc287ceb07f2a4f364aa04745624230d
EndeeerMan <endeeerman@gmail.com>
2025-11-19 20:42:48 +0800

    全体起立！__int128 输入输出实现！

--- _int128使用/int128test.c
+++ _int128使用/int128test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 void print_int128(__int128 num){
@@ -6,33 +7,41 @@ void print_int128(__int128 num){
         printf("0\n");
         return;
     }
-    if(num < 0){
-        putchar('-');
-        num = -num;
-    }
-    char arr[50];
-    int counter = 0;
+    if(num < 0){putchar('-');num = -num;};
+    int arr[50];
+    int i = 0;
     while(num > 0){
-        arr[counter++] = '0' + (num % 10);
+        arr[i++] = num % 10;
         num /= 10;
     }
-    for(int i=counter-1;i>=0;i--){
-        putchar(arr[i]);
+    for(i-=1;i>=0;i--){
+        printf("%d",arr[i]);
     }
-    printf("\n");
+    return;
 }
 
-__int128 read_int128(char *cnum){
+__int128 read_int128_from_string(char *cnum){
     __int128 num = 0;
+    cnum = &cnum[0];
+    while (*cnum == ' ' || *cnum == '\t' || *cnum == '\n') cnum++;
+    if(*cnum == '0') return 0;
     int flag = 1;
-    if(*cnum == '-') flag = -1;
-    int i = 0;
-    while(isdigit(cnum[i++])){
-
+    if(*cnum == '-'){cnum++;flag = -1;}
+    if(*cnum == '+') cnum++;
+    int digit = 0;
+    while(*cnum != '\0'){
+        digit = *cnum++ - '0';
+        num = num*10 + digit;
     }
+    return num*flag;
 }
 
 int main(){
-    const char *num1 = "17014118346046923173168730371588410572";
-    __int128 num = 
+    char *cnum = NULL;
+    cnum = malloc(sizeof(char)*51);
+    scanf("%50s",cnum);
+    __int128 num = read_int128_from_string(cnum);
+    print_int128(num);
+    putchar('\n');
+    return 0;
 }
